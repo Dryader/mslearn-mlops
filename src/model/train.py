d3d0864ac1e413eafc38af13dee6e4bb881cac07
Dryader <@>
2026-02-16 10:56:41 -0500

    Challenge 0

--- src/model/train.py
+++ src/model/train.py
@@ -2,17 +2,19 @@
 
 import argparse
 import glob
+import mlflow
 import os
 
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -34,6 +36,11 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df):
+    X, y = df.drop('Diabetic', axis=1).values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
+    
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
